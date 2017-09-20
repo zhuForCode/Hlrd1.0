6aaf52031e402c5327cc862fad9132a31ba378cc
J.Kuai <congjinruo@outlook.com>
2017-09-20 16:24:03 +0800

    HLRD-1.0.3 基本可用版本

--- app/src/main/java/com/juhua/hangfen/eedsrd/activity/LoginActivity.java
+++ app/src/main/java/com/juhua/hangfen/eedsrd/activity/LoginActivity.java
@@ -9,6 +9,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -29,6 +30,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -303,6 +305,17 @@ public class LoginActivity extends Activity {
         });
 
     }
+
+    //此方法只是关闭软键盘
+    private void hintKbTwo() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(imm.isActive()&&getCurrentFocus()!=null){
+            if (getCurrentFocus().getWindowToken()!=null) {
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        }
+    }
+
     TextWatcher mTextWatcher  = new TextWatcher() {
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
--- app/src/main/java/com/juhua/hangfen/eedsrd/constants/Constants.java
+++ app/src/main/java/com/juhua/hangfen/eedsrd/constants/Constants.java
@@ -13,10 +13,10 @@ public interface Constants {
     String VERIFY = "+afJ7yNqhDL67Hbo7ib9HQ==";
     int REQUEST_TIMEOUT = 8000;
 
-    String DOMAIN_NAME = "http://192.168.1.141/";
+    String DOMAIN_NAME = "http://188.3.10.72:803/";
     String WEBSERVICE_URL = " /WebServers/AppSer.asmx";
     String NAME_SPACE = "http://tempuri.org/";
-    String WSDL = "http://192.168.1.141/WebServers/AppSer.asmx?WSDL";
+    String WSDL = "http://188.3.10.72:803/WebServers/AppSer.asmx?WSDL";
     String METHOD_APP_LOGIN = "AppLoginForZjrd";
     String METHOD_GET_APP_VERSION = "GetWebAppVersion";
     String METHOD_GET_BACK_PASSWORD = "GetBackPassWord";
